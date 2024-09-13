86a4945359c36608d1cb2156085a8beed03359d2
Daniel Lopez <d@niel.to>
2024-09-12 18:12:16 -0700

    update title styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
   return (
     <Main className='flex flex-col p-4'>
       <div className='flex flex-grow flex-col space-y-4 md:items-center'>
-        <Title>5x5</Title>
+        <div className='flex'>
+          <Title>5x5</Title>
+        </div>
         <div className='grid gap-4 md:grid-cols-2'>
           {exercises.map(exercise => (
             <WarmupCalculator key={exercise} exercise={exercise} />
